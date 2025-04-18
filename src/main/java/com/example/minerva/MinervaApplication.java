ac0d0a43e35477826d04159a0f498386aaeaeb87
Makan Sepehrifar <makan1869@gmail.com>
2025-04-18 09:40:20 +0200

    Add main application class

---
+++ src/main/java/com/example/minerva/MinervaApplication.java
@@ -0,0 +1,12 @@
+package com.example.minerva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinervaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinervaApplication.class, args);
+    }
+}
