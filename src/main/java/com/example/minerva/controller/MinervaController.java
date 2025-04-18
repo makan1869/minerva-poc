28aac1dfad9286bfbd8695ade654d0891a36be39
Makan Sepehrifar <makan1869@gmail.com>
2025-04-18 09:40:25 +0200

    Add controller class

---
+++ src/main/java/com/example/minerva/controller/MinervaController.java
@@ -0,0 +1,15 @@
+package com.example.minerva.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MinervaController {
+
+    @GetMapping("/status")
+    public String getStatus() {
+        return "Minerva PoC is running!";
+    }
+}
